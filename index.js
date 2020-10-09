e0114d5e8accf4f36c60bec7e209e17a109da5b2
akhilesoni <soniakhilesh101@gmail.com>
2020-10-09 15:27:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+//loading server
+const express = require('express');
+const app = express();
+var mysql = require('mysql');
+var path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+app.engine('ejs',require('ejs').__express)
+app.use(express.static('public'));
+
+var db = mysql.createConnection({
+    host:"127.0.0.1",
+    user:"root",
+    password:"akhi2111@",
+    database:"images_database"
+});
+
+db.connect((err) => {
+    if(err) console.log(err);
+    else console.log("connected");
+})
+//routes
+app.get('/',(req,res)=>{
+    db.query('select * from images ;',(err,result) =>{
+        if(err){
+            res.render('error',{message:"fetch failed : internal server error :("})
+        }
+        else{
+            
+            res.render('index',{images:result})
+        }
+    })
+})
+
+app.get('/image:id', (req,res)=>{
+    var id = req.params.id;
+    db.query('select * from images;', (err,result)=>{
+        if(err){
+            console.log(err)
+            res.render('error',{message:"internal sever error"})
+        }
+        else{
+            console.log(result);
+            res.render('image',{images:result,id:id})
+        }
+    })
+})
+//port listening
+app.listen(2000, ()=>{
+    console.log("server running");
+});
